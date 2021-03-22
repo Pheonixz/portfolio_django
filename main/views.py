722b7353dae48a534e621fdbb87d0c19dc6d60b0
Pheonixz <YcM-wu9-7av-HWD>
2021-03-22 20:13:38 +0300

    python django simple website, v.0.1.

---
+++ main/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+
+def index(request):
+    tasks = Task.objects.order_by("id")
+    return render(request, "main/index.html", {"title": "Main", "tasks": tasks})
+
+
+def about(request):
+    return render(request, "main/about.html")
+
+
+def create_task(request):
+    error = None
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+        else:
+            error = "Форма была неверной"
+
+    form = TaskForm()
+    context = {
+        "form": form,
+        "error": error
+    }
+
+    return render(request, "main/create_task.html", context)
+
+
